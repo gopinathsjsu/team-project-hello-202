873ea4dcf44b59085a30b4d5ba5299fb8de71b15
kfbustam <karoljosefb@gmail.com>
2022-04-10 23:46:53 -0700

    update Trips pagination

--- src/client/components/Trips.jsx
+++ src/client/components/Trips.jsx
@@ -1,8 +1,8 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import "react-datetime/css/react-datetime.css";
 import Image from 'react-bootstrap/Image'
-import ListGroup from 'react-bootstrap/ListGroup';
+import Card from 'react-bootstrap/Card';
 import Pagination from 'react-bootstrap/Pagination';
 
 const TripsStyle = {
@@ -21,35 +21,62 @@ const backgroundImageStyle = {
   width: '100vw',
 };
 
-const MIN_ROOM_COUNT = 0;
-const MAX_ROOM_COUNT = 10;
-const MIN_ADULT_COUNT = 0;
-const MAX_ADULT_COUNT = 10;
-const MIN_CHILDREN_COUNT = 0;
-const MAX_CHILDREN_COUNT = 10;
+const NUM_CARDS_PER_PAGE = 6;
+const MAX_PAGINATION_COUNT = 5;
+const TRIPS_TOTAL_COUNT = 36;
 
 function Trips(props) {
-  const onSubmitClick = () => {
-    fetch('http://localhost:8080/getRooms', {
+  const pages = [];
+  const trips = [];
+  const [active, setActive] = useState(1);
+  const [epi, setEpi] = useState([]);
+  const totalEpis = [];
+
+  let indOfLastEpi = active * NUM_CARDS_PER_PAGE;
+  let indOfFirstEpi = indOfLastEpi - NUM_CARDS_PER_PAGE;
+
+  for (let number = 1; number <= TRIPS_TOTAL_COUNT; number++) {
+    totalEpis.push(number);
+  }
+
+  for (let number = 1; number <= trips.length; number++) {
+    totalEpis.push(number);
+  }
+
+  const pagination = (number) => {
+    indOfLastEpi = number * NUM_CARDS_PER_PAGE;
+    indOfFirstEpi = indOfLastEpi - NUM_CARDS_PER_PAGE;
+    setActive(number);
+    fetch("https://rickandmortyapi.com/api/episode/" + totalEpis.slice(indOfFirstEpi, indOfLastEpi), {
       headers: {
         'Content-Type': 'application/json'
       },
-      method: 'POST',
-      body: JSON.stringify({
-        adultCount, checkInDate, checkOutDate, childrenCount, destination, roomCount
-      })
+      method: 'GET',
     })
       .then((res) => {
-        if (res.ok) {
-          return res.json().then((responseData) => {
-            const { rooms } = responseData;
-            // TODO: Set rooms and change page view
-            return responseData;
-          });
-        }
-        console.log('Error occurred:');
-        console.log(res);
-        return { errorMessages: { REQUEST_ERROR: res.statusText } };
+        res.json().then((data) => {
+          // setEpi(data);
+          setEpi([
+            {
+              id: 1,
+              name: 'The Ritz-Carlton Residences, Waikiki Beach',
+              air_date: 'December 2, 2013',
+              episode: '383 Kalaimoku Street Waikiki Beach, Hawaii 96815',
+            },
+            {
+              id: 2,
+              name: 'The Ritz-Carlton Residences, Waikiki Beach',
+              air_date: 'December 2, 2013',
+              episode: '383 Kalaimoku Street Waikiki Beach, Hawaii 96815',
+            },
+            {
+              id: 3,
+              name: 'The Ritz-Carlton Residences, Waikiki Beach',
+              air_date: 'December 2, 2013',
+              episode: '383 Kalaimoku Street Waikiki Beach, Hawaii 96815',
+            },
+          ]);
+        });
       })
       .catch((exception) => {
         console.log('Error occurred:');
@@ -57,61 +84,101 @@ function Trips(props) {
       });
   }
 
-  const checkInDateTimeInputProps = {
-    placeholder: 'Check In'
-  };
-  const checkOutDateTimeInputProps = {
-    placeholder: 'Check Out'
-  };
+  useEffect(() => {
+    if (active === 1) {
+      fetch("https://rickandmortyapi.com/api/episode/" + totalEpis.slice(indOfFirstEpi, indOfLastEpi), {
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        method: 'GET',
+      })
+        .then((res) => {
+          res.json().then((data) => {
+            // setEpi(data);
+            setEpi([
+              {
+                id: 1,
+                name: 'The Ritz-Carlton Residences, Waikiki Beach',
+                air_date: 'December 2, 2013',
+                episode: '383 Kalaimoku Street Waikiki Beach, Hawaii 96815',
+              },
+              {
+                id: 2,
+                name: 'The Ritz-Carlton Residences, Waikiki Beach',
+                air_date: 'December 2, 2013',
+                episode: '383 Kalaimoku Street Waikiki Beach, Hawaii 96815',
+              },
+              {
+                id: 3,
+                name: 'The Ritz-Carlton Residences, Waikiki Beach',
+                air_date: 'December 2, 2013',
+                episode: '383 Kalaimoku Street Waikiki Beach, Hawaii 96815',
+              },
+            ]);
+          });
+        })
+        .catch((exception) => {
+          console.log('Error occurred:');
+          console.log(exception);
+        });
+    }
+  }, [active]);
+
+  for (let number = 1; number <= MAX_PAGINATION_COUNT; number++) {
+    pages.push(
+      <Pagination.Item
+        key={number}
+        active={number === active}
+        onClick={() => pagination(number)}
+      >
+        {number}
+      </Pagination.Item>
+    );
+  }
 
   return (
     <div style={rootStyle}>
       <Image src="https://images.unsplash.com/photo-1543731068-7e0f5beff43a" style={backgroundImageStyle} />
-      <ListGroup as="ol" numbered style={{ margin: '0vh auto 5vh auto', width: '90%' }}>
-        <ListGroup.Item
-          as="li"
-          className="d-flex justify-content-between align-items-start"
-        >
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">The Ritz-Carlton Residences, Waikiki Beach</div>
-            383 Kalaimoku Street Waikiki Beach, Hawaii 96815
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item
-          as="li"
-          className="d-flex justify-content-between align-items-start"
-        >
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">The Ritz-Carlton Residences, Waikiki Beach</div>
-            383 Kalaimoku Street Waikiki Beach, Hawaii 96815
-          </div>
-        </ListGroup.Item>
-        <ListGroup.Item
-          as="li"
-          className="d-flex justify-content-between align-items-start"
-        >
-          <div className="ms-2 me-auto">
-            <div className="fw-bold">The Ritz-Carlton Residences, Waikiki Beach</div>
-            383 Kalaimoku Street Waikiki Beach, Hawaii 96815
-          </div>
-        </ListGroup.Item>
-      </ListGroup>
+      <div style={{ margin: 'auto' }}>
+        {epi && epi.map(ep => (
+          <Card
+            key={ep.id}
+            style={{ width: "75vw", margin: "2rem", textAlign: "center" }}
+          >
+            <Card.Body>
+              <Card.Title>
+                <span style={{ fontWeight: "bold" }}>Episode:</span> {ep.name}
+              </Card.Title>
+              <Card.Text>
+                <span style={{ fontWeight: "bold" }}>Ep. Code:</span>{" "}
+                {ep.episode}
+              </Card.Text>
+              <Card.Text>
+                <span style={{ fontWeight: "bold" }}>Date:</span> {ep.air_date}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+        ))}
+      </div>
       <Pagination style={{ margin: 'auto' }}>
-        <Pagination.First />
-        <Pagination.Prev />
-        <Pagination.Item>{1}</Pagination.Item>
-        <Pagination.Ellipsis />
-
-        <Pagination.Item>{10}</Pagination.Item>
-        <Pagination.Item>{11}</Pagination.Item>
-        <Pagination.Item active>{12}</Pagination.Item>
-        <Pagination.Item>{13}</Pagination.Item>
-        <Pagination.Item disabled>{14}</Pagination.Item>
-
-        <Pagination.Ellipsis />
-        <Pagination.Item>{20}</Pagination.Item>
-        <Pagination.Next />
-        <Pagination.Last />
+        <Pagination.First onClick={() => {
+          pagination(1);
+        }} />
+        <Pagination.Prev onClick={() => {
+          if (active > 1) {
+            pagination(active - 1);
+          }
+        }} />
+        {pages}
+        <Pagination.Next
+          onClick={() => {
+            if (active < 5) {
+              pagination(active + 1);
+            }
+          }} />
+        <Pagination.Last onClick={() => {
+          pagination(5);
+        }} />
       </Pagination>
     </div >
   );
