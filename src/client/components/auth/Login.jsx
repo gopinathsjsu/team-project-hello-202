42cfc323c4826a5189907d504364ac8f41b785a6
kfbustam <karoljosefb@gmail.com>
2022-04-14 21:53:24 -0700

    refactor: Update Signup and login components

--- src/client/components/auth/Login.jsx
+++ src/client/components/auth/Login.jsx
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "react-bootstrap/Form";
 import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
 
 const formStyle = {
   margin: "auto",
@@ -9,24 +10,56 @@ const formStyle = {
 };
 
 function Login() {
+  const [password, setPassword] = useState();
+  let navigate = useNavigate();
+
+  const setPasswordInput = (e) => {
+    setPassword(e.target.value);
+  }
+  const setEmailInput = (e) => {
+    setEmail(e.target.value)
+  }
+
+
+  const authenticateUser = () => {
+    fetch("http://Hmanage-env.eba-ibcrgcpt.us-east-2.elasticbeanstalk.com/user", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        password,
+        email,
+      }),
+    })
+      .then((res) => {
+        res.json().then((data) => {
+          navigate('/dashboard')
+        })
+          .catch((exception) => {
+            console.log("Error occurred:")
+            console.log(exception)
+          });
+      })
+  }
   return (
     <Form style={formStyle}>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
+      <Form.Group className="mb-3" controlId="email">
         <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
+        <Form.Control type="email" placeholder="Enter email" onChange={setEmailInput} />
         <Form.Text className="text-muted">
           We'll never share your email with anyone else.
         </Form.Text>
       </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
+      <Form.Group className="mb-3" controlId="password" >
         <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
+        <Form.Control type="password" placeholder="Password" onChange={setPasswordInput} />
       </Form.Group>
       <Form.Group className="mb-3" controlId="formBasicCheckbox">
         <Form.Check type="checkbox" label="Check me out" />
       </Form.Group>
-      <Button variant="primary" type="submit">
+      <Button variant="primary" type="submit" onClick={authenticateUser}>
         Submit
       </Button>
     </Form>
--- src/client/components/auth/SignUp.jsx
+++ src/client/components/auth/SignUp.jsx
@@ -1,40 +1,84 @@
-import React from "react";
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import { Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+
+const formStyle = {
+  margin: "auto",
+  width: "50vw",
+  height: "50vh",
+};
 
 function SignUp() {
+  const [name, setName] = useState();
+  const [password, setPassword] = useState();
+  const [email, setEmail] = useState();
+  let navigate = useNavigate();
+
+  const setPasswordInput = (e) => {
+    setPassword(e.target.value);
+  }
+  const setEmailInput = (e) => {
+    setEmail(e.target.value)
+  }
+  const setNameInput = (e) => {
+    setName(e.target.value);
+  }
+
+  const signUpUser = () => {
+    fetch("http://Hmanage-env.eba-ibcrgcpt.us-east-2.elasticbeanstalk.com/user", {
+      headers: {
+        "Content-Type": "application/json",
+      },
+      method: "POST",
+      body: JSON.stringify({
+        name,
+        password,
+        email,
+      }),
+
+    })
+      .then((res) => {
+        res.json().then((data) => {
+          console.log('success');
+          navigate('/login');
+        })
+          .catch((exception) => {
+            console.log("Error occurred:");
+            console.log(exception);
+          });
+      })
+  }
   return (
-    <form>
-      <h3>Sign Up</h3>
-      <div className="form-group">
-        <label>First name</label>
-        <input type="text" className="form-control" placeholder="First name" />
-      </div>
-      <div className="form-group">
-        <label>Last name</label>
-        <input type="text" className="form-control" placeholder="Last name" />
-      </div>
-      <div className="form-group">
-        <label>Email address</label>
-        <input
-          type="email"
-          className="form-control"
-          placeholder="Enter email"
-        />
-      </div>
-      <div className="form-group">
-        <label>Password</label>
-        <input
-          type="password"
-          className="form-control"
-          placeholder="Enter password"
-        />
-      </div>
-      <button type="submit" className="btn btn-primary btn-block">
-        Sign Up
-      </button>
-      <p className="forgot-password text-right">
-        Already registered <a href="#">sign in?</a>
-      </p>
-    </form>
+    <>
+      <Form style={formStyle}>
+        <Form.Group className="mb-3" controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control type="name" placeholder="Enter name" onChange={setNameInput} />
+          <Form.Text className="text-muted">
+            We'll never share your name with anyone else.
+          </Form.Text>
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="email">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control type="email" placeholder="Enter email" onChange={setEmailInput} />
+          <Form.Text className="text-muted">
+            We'll never share your email with anyone else.
+          </Form.Text>
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="password" >
+          <Form.Label>Password</Form.Label>
+          <Form.Control type="password" placeholder="Password" onChange={setPasswordInput} />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicCheckbox">
+          <Form.Check type="checkbox" label="Check me out" />
+        </Form.Group>
+      </Form>
+      <Button variant="primary" type="submit" onClick={signUpUser} style={{ margin: 'auto' }}>
+        Submit
+      </Button>
+    </>
   );
 }
 
